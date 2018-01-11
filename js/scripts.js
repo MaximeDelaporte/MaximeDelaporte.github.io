84fea79bdb1875d9462e98bc8fe6adaf306a5e10
MaximeDelaporte <delaporte.maxime@orange.fr>
2018-01-11 22:33:27 +0100

    header imagé

--- js/scripts.js
+++ js/scripts.js
@@ -16,19 +16,20 @@
       $('nav ul').toggle();
     });
   });
-  var parallaxElements = $('.parallax'),
-  parallaxQuantity = parallaxElements.length;
 
-  $(window).on('scroll', function() {
-    window.requestAnimationFrame(function(){
-      for (var i = 0; i < parallaxQuantity; i ++) {
-        var currentElement = parallaxElements.eq(i);
-        var scrolled = $(window).scrollTop();
-
-        currentElement.css({
-          'transform': 'translate3d(0,' + scrolled * -0.3 + 'px, 0)'
-        });
-      }
-    });
-  });
 })(jQuery);
+var parallaxElements = $('.parallax'),
+parallaxQuantity = parallaxElements.length;
+
+$(window).on('scroll', function() {
+  window.requestAnimationFrame(function(){
+    for (var i = 0; i < parallaxQuantity; i ++) {
+      var currentElement = parallaxElements.eq(i);
+      var scrolled = $(window).scrollTop();
+
+      currentElement.css({
+        'transform': 'translate3d(0,' + scrolled * -0.3 + 'px, 0)'
+      });
+    }
+  });
+});
